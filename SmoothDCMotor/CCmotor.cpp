456aaa75becf0629cbc0b34d0acb75432c85af2b
HRN88 <gustavoheras.h@gmail.com>
2017-08-31 10:26:25 -0500

    Version 0.0
    Libreria CCmotor,h para giro suave

---
+++ SmoothDCMotor/CCmotor.cpp
@@ -0,0 +1,100 @@
+#include "CCmotor.h"
+#include <Arduino.h>
+
+CCMotor::CCMotor(int pin1, int pin2){
+  ctrla = pin1;
+  ctrlb = pin2;
+  
+}
+void CCMotor::SetAcel(int acel){
+  aceleracion = acel;
+}
+
+void CCMotor::SetVini(int vini){
+  velini = vini;
+  velact = vini;
+}
+
+void CCMotor::SetVmax(int vmax){
+  velmax = vmax;
+}
+
+void CCMotor::SetParams(int acel, int vini, int vmax){
+  aceleracion = acel;
+  velini = vini;
+  velact = vini;
+  velmax = vmax;
+}
+
+void CCMotor::GetAllParams(){
+  Serial.print("Velocidad actual: ");
+  Serial.print(velini);
+  Serial.print(" Velocidad maxima: ");
+  Serial.print(velmax);
+  Serial.print(" Aceleracion: ");
+  Serial.print(aceleracion);
+  Serial.print(" Pines de conexion: ");
+  Serial.print(ctrla);
+  Serial.print(" y ");
+  Serial.println(ctrlb);
+}
+
+void CCMotor::GetVelocidadActual(){
+  Serial.println(velini);
+
+}
+
+void CCMotor::acelera(){
+  tiempoActual = millis();
+  if((tiempoActual - tiempoAnterior) >= 50){
+    tiempoAnterior = tiempoActual;
+    if(velini <= velmax){
+    velini = velini + aceleracion*tiempo;
+    tiempo ++;
+    }
+    Serial.println(velini);
+  }
+
+  
+  
+ 
+}
+
+void CCMotor::Gira(char direccion){
+  anteriorMotor = direccion;
+  if(anteriorMotor != actualMotor){
+    velini = velact;
+    tiempo=0;
+  }
+  switch(direccion){
+    
+    case '1':
+      actualMotor = '1';
+      acelera();
+      analogWrite(ctrla, velini);
+      analogWrite(ctrlb, 0);
+      break;
+      
+    case '2':
+      actualMotor = '2';
+      acelera();
+      analogWrite(ctrla, 0);
+      analogWrite(ctrlb, velini);
+      break;
+      
+    default:
+      parar(); 
+      parar();
+      break;
+      
+      }
+ 
+}
+
+void CCMotor::parar(){
+  tiempo = 0;
+  analogWrite(ctrla, 0);
+  analogWrite(ctrlb, 0);
+}
+
+
---
+++ SmoothDCMotor/CCmotor.h
@@ -0,0 +1,64 @@
+////////////////////////////////
+//        
+//Definicion de la clase CCMotor
+//
+////////////////////////////////
+
+class CCMotor{
+  private:
+    //Aceleracion
+    int aceleracion;
+    //Velocidad minima y maxima de 0-255
+    int velini;
+    int velact;
+    int velmax;
+    int timpo;
+    //Pines de control para el motor
+    int ctrla;
+    int ctrlb;
+    //Variable temporal
+    int tiempo = 0;
+    char anteriorMotor;
+    char actualMotor;
+    //tiempos para el timer
+    unsigned long tiempoActual;
+    unsigned long tiempoAnterior;
+    
+
+   public:
+   //Constructor, crea el objeto requiere los pines de control
+   CCMotor(int pin1, int pin2);
+   
+   //Funciones Set, para establecer:
+   //aceleracion
+   void SetAcel(int acel);
+   
+   //Velocidad inicial
+   void SetVini(int vini);
+   
+   //Velocidad final maxima
+   void SetVmax(int vmax);
+   
+   //Todos los parametros
+   void SetParams(int acel, int vini, int vmax); 
+
+   //Solicita los parametros establecidos y los imprime por serial (115200)BAUDS
+   void GetAllParams();
+
+   void GetVelocidadActual();
+
+    //Metodo privado que calcula la velocidad en funcion de la aceleracion
+   void acelera();
+
+   //Funcion para iniciar giro
+   void Gira(char direccion);
+
+   //Funcion Para parar el motor
+
+   void parar();
+  
+};
+
+
+
+
