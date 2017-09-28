656209dd0d7d3b77ee4454bddf62a06bb8b6a2d7
HRN88 <gustavoheras.h@gmail.com>
2017-09-28 14:08:06 -0500

    Actualizacion 28/09/2017

--- core/core.ino
+++ core/core.ino
@@ -1,3 +1,4 @@
+
 /*******************************************************/
 /*************Version con motor a pasos*****************/
 /*******************************************************/
@@ -7,8 +8,8 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-
+#include <AccelStepper.h>
+#include <MultiStepper.h>
 
 //Constantes para la comunicacion 
 #define CE_PIN 9
@@ -20,6 +21,13 @@
 #define a3 17
 #define a4 16
 
+//************Pines para el control del motor superior***********
+#define p1 23
+#define p2 22
+#define p3 20
+#define p4 6
+
+
 //Se guarda la direccion que se usa para establecer la comunicacione entre ambos
 //tranceprores por especificacion de la documentacion se  usa un vector
 byte direccion[4] = {'f', 'u', 'n', 'k'};
@@ -28,6 +36,7 @@ char dato[3];
 //Variable para recibir entrada de teclado (Solo aplica para pruebas por cable USB)
 char input;
 
+
 //**********Instancia de los objetos***********
 //Instancias de motores
 CCMotor motor(a1, a2);
@@ -35,6 +44,10 @@ CCMotor motor2(a3, a4);
 
 //Instancia de modulo NRF24L01
 RF24 receptor(CE_PIN, CSN_PIN);
+
+//Se crea una instancia del motor a pasos, el numero 4, indica que se
+//esta trabajando en modo Full4Wire
+AccelStepper motorp(4, p4, p2, p3, p1);
 
 void setup() {
   // put your setup code here, to run once:
@@ -49,71 +62,100 @@ void setup() {
   /*Se establecen los parametros iniciales para cada par de motores
    * aceleracion, velocidad inicial y velocidad final, respectivamente
    */
-   motor.SetParams(4,100,255);
-   motor2.SetParams(4,100,255);
+   motor.SetParams(4,90,255);
+   motor2.SetParams(4,90,255);  
+   
+  //Establece la velocidad maxima del motor a pasos
+   motorp.setMaxSpeed(200);
+  //Establece la velocidad inicial
+   motorp.setSpeed(20);
+  //Establece la aceleracion
+   motorp.setAcceleration(80);
 }
 
+//Funcion para mover el motor a pasos
+void buclemotor();
 
 void loop() {
    //put your main code here, to run repeatedly
    
    //Se recibe el dato por el bufer de la comunicacion inalambrica
-   if(receptor.available()){
-    receptor.read(dato, sizeof(dato));
-    Serial.print("El estado es: ");
-    Serial.println(dato[0]);
-   }
-   else{
-    Serial.println("No hay nada");
-   }
+//   if(receptor.available()){
+//    receptor.read(dato, sizeof(dato));
+//    Serial.print("El estado es: ");
+//    Serial.println(dato[0]);
+//   }
+//   else{
+//    Serial.println("No hay nada");
+//   }
 
  //*********************Para el caso de prubeas por cable usb*********************
  //Se recibe el dato del teclado
  
-//   if(Serial.available()){
-//    input = Serial.read();
-//    
-//   }
+   if(Serial.available()){
+    input = Serial.read();
+    
+   }
 //********************************************************************************
 
-   switch(dato[0]){
+   switch(input){
 
     //****************Estados de Giro*****************
       case 'w':
       //Adelante
-        motor.Gira('2'); 
-        motor2.Gira('2');
+        motor.Gira('1'); 
+        motor2.Gira('1');
       break;
       
       case 's':
       //Atras
-        motor.Gira('1'); 
-        motor2.Gira('1');
+        motor.Gira('2'); 
+        motor2.Gira('2');
       break;
 
       case 'a':  
       //Izquierda   
-        motor.Gira('2'); 
-        motor2.Gira('1');
+        motor.Gira('1'); 
+        motor2.Gira('2');
       break;
 
       case 'd':
       //Derecha
-        motor.Gira('1'); 
-        motor2.Gira('2');
+        motor.Gira('2'); 
+        motor2.Gira('1');
       break;
   //******************************************************
 
+      case '1':
+      motorp.move(100);
+      buclemotor();
+      break;
+
+      case '2':
+      motorp.move(-100);
+      buclemotor();
+      break;
+      
+
+       
+        
     
   //Caso default, el sistema permanece inmovil   
     default:
       motor.parar(); 
       motor2.parar();
-      break;
-    
+      break;   
     
    }
    
+}
+
+void buclemotor(){
+  do{
+      motorp.run();
+  }while(motorp.distanceToGo() != 0);
+      motorp.stop();
+      motorp.setCurrentPosition(0);
 }
   
 
